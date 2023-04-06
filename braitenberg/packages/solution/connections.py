5e73d65f7cef909d19b361e2825a4d2d5c692ecf
christian <chrismobile@bluewin.ch>
2023-04-06 14:39:05 +0200

    Modifications according to exercises-test #98609 (17.6.2021) 	modified:   notebooks/01-Image-Manipulation/braitenberg01.ipynb 	modified:   notebooks/02-Image-Filtering/braitenberg02.ipynb 	modified:   notebooks/03-Braitenberg/braitenberg03.ipynb 	modified:   packages/braitenberg_agent/agent.py 	modified:   packages/solution/connections.py 	modified:   packages/solution/preprocessing.py

--- braitenberg/packages/solution/connections.py
+++ braitenberg/packages/solution/connections.py
@@ -7,9 +7,11 @@ def get_motor_left_matrix(shape: Tuple[int, int]) -> np.ndarray:
     # TODO: write your function instead of this one
     res = np.zeros(shape=shape, dtype="float32")
     # these are random values
-    res[100:150, 100:150] = 1
-    res[300:, 200:] = 1
-    # ---
+    #res[100:150, 100:150] = 1
+    #res[300:, 200:] = 1
+    # this is my proposal
+    res[240: , :320] = 1    # close encounter on left side -> accelerates left wheel
+    #res[240: , 321: ] = -1   # close encounter on right side -> decelerates left wheel
     return res
 
 
@@ -17,6 +19,8 @@ def get_motor_right_matrix(shape: Tuple[int, int]) -> np.ndarray:
     # TODO: write your function instead of this one
     res = np.zeros(shape=shape, dtype="float32")
     # these are random values
-    res[100:150, 100:300] = -1
-    # ---
+    # res[100:150, 100:300] = -1
+    # this is my proposal
+    res[240: , 321: ] = 1   # close encounter on right side -> accelerates right wheel
+    #res[240: , :320] = -1  # close encounter on left side -> decelerates right wheel
     return res
