6b2e3bebba6fae08220a4a57b5506550bbeb95e0
christian <chrismobile@bluewin.ch>
2023-04-07 19:58:23 +0200

    tripartite response area in midground, no foreground response 	modified:   notebooks/03-Braitenberg/braitenberg03.ipynb 	modified:   packages/braitenberg_agent/agent.py 	modified:   packages/solution/connections.py 	modified:   packages/solution/preprocessing.py

--- braitenberg/packages/solution/connections.py
+++ braitenberg/packages/solution/connections.py
@@ -9,10 +9,10 @@ def get_motor_left_matrix(shape: Tuple[int, int]) -> np.ndarray:
     # these are random values
     #res[100:150, 100:150] = 1
     #res[300:, 200:] = 1
-    # this is my proposal
-    #res[240: , :320] = 1    # close encounter on left side -> accelerates left wheel
-    #res[240: , 321: ] = -1   # close encounter on right side -> decelerates left wheel
-    res[240: , 321: ] = -1   # close encounter on right side -> decelerates left wheel
+    # this is my proposal for the 3fold divided mask:
+    res[240:360, :200] = 1
+    res[240:360, 200:440] = -1
+    res[240:360, 441: ] = -1
     return res
 
 
@@ -21,8 +21,8 @@ def get_motor_right_matrix(shape: Tuple[int, int]) -> np.ndarray:
     res = np.zeros(shape=shape, dtype="float32")
     # these are random values
     # res[100:150, 100:300] = -1
-    # this is my proposal
-    #res[240: , 321: ] = 1   # close encounter on right side -> accelerates right wheel
-    #res[240: , :320] = -1  # close encounter on left side -> decelerates right wheel
-    res[240: , :320] = -1    # close encounter on left side -> decelerates right wheel
+    # this is my proposal for the 3fold divided mask:
+    res[240:360 , :200] = -1
+    res[240:360 , 200:440] = -1
+    res[240:360 , 441: ] = 1
     return res
