e2c37725295f2f3e333890819d2fe1d1bfcb0645
christian <chrismobile@bluewin.ch>
2023-04-16 16:44:17 +0200

    a new start: left and right lower half masks, c=0.1, g=0.1, linear

--- braitenberg/packages/solution/connections.py
+++ braitenberg/packages/solution/connections.py
@@ -8,11 +8,9 @@ def get_motor_left_matrix(shape: Tuple[int, int]) -> np.ndarray:
     res = np.zeros(shape=shape, dtype="float32")
     # these are random values
     #res[100:150, 100:150] = 1
-    #res[300:, 200:] = 1
-    # this is my proposal for the 3fold divided mask:
-    res[240:360, :200] = 1
-    res[240:360, 200:440] = -1
-    res[240:360, 441: ] = -1
+    #res[300: ,200:] = 1
+    # this is my proposal for the left half mask:
+    res[240: , :320] = 1
     return res
 
 
@@ -21,8 +19,6 @@ def get_motor_right_matrix(shape: Tuple[int, int]) -> np.ndarray:
     res = np.zeros(shape=shape, dtype="float32")
     # these are random values
     # res[100:150, 100:300] = -1
-    # this is my proposal for the 3fold divided mask:
-    res[240:360 , :200] = -1
-    res[240:360 , 200:440] = -1
-    res[240:360 , 441: ] = 1
+    # this is my proposal for the right half mask:
+    res[240:  ,321: ] = 1
     return res
