819fb8c4d61d7e3f1c4ac569cc88af8ff075108c
christian <chrismobile@bluewin.ch>
2023-04-16 20:43:21 +0200

    mask left and right in middleground only

--- braitenberg/packages/solution/connections.py
+++ braitenberg/packages/solution/connections.py
@@ -10,7 +10,7 @@ def get_motor_left_matrix(shape: Tuple[int, int]) -> np.ndarray:
     #res[100:150, 100:150] = 1
     #res[300: ,200:] = 1
     # this is my proposal for the left half mask:
-    res[240: , :320] = 1
+    res[240:340, :320] = 1
     return res
 
 
@@ -20,5 +20,5 @@ def get_motor_right_matrix(shape: Tuple[int, int]) -> np.ndarray:
     # these are random values
     # res[100:150, 100:300] = -1
     # this is my proposal for the right half mask:
-    res[240:  ,321: ] = 1
+    res[240:340,321: ] = 1
     return res
